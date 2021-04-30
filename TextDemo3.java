12c7169f5a7276340b85b58db12ff74eb20fc4cd
sqissuper <1764491917@qq.com>
2021-04-30 12:54:20 +0800

    dp刷题

---
+++ TextDemo3.java
@@ -0,0 +1,59 @@
+public class TextDemo3 {
+    //扑克牌
+//    public static String[] getBigBrand(String[] left,String[] right) {
+//        HashMap<String,Integer> map = new HashMap<>();
+//        map.put("3",3);
+//        map.put("4",4);
+//        map.put("5",5);
+//        map.put("6",6);
+//        map.put("7",7);
+//        map.put("8",8);
+//        map.put("9",9);
+//        map.put("10",10);
+//        map.put("J",11);
+//        map.put("Q",12);
+//        map.put("k",13);
+//        map.put("A",14);
+//        map.put("2",15);
+//        map.put("joker",16);
+//        map.put("JOKER",17);
+//        int l = left.length,r = right.length;
+//        switch(l){
+//            case 1:
+//                return func(map,left,right);
+//            case 2:
+//                return func(map,left,right);
+//            case 3:
+//                return func(map,left,right);
+//            case 4:
+//                return func(map,left,right);
+//            case 5:
+//                return func(map,left,right);
+//        }
+//        return new String[]{"ERROR"};
+//    }
+//    public static String[] func(HashMap<String,Integer> map,String[] left,String[] right) {
+//        if(map.get(left[0]) < map.get(right[0])) {
+//            return right;
+//        }
+//        return left;
+//    }
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        while(sc.hasNextInt()) {
+//            String brand = sc.nextLine();
+//            String[] tmp = brand.split("-");
+//            if(brand.contains("joker JOKER")) {
+//                System.out.println("joker JOKER");
+//            } else {
+//                String[] left = tmp[0].split(" ");
+//                String[] right = tmp[1].split(" ");
+//                String[] result = getBigBrand(left,right);
+//                for(int i = 0; i < result.length; i++) {
+//                    System.out.print(result[i] + " ");
+//                }
+//            }
+//
+//        }
+    }
+}
